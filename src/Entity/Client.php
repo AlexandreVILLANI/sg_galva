057e04fcb173ec6b59a1fef0725f45b097ffa234
Alexandre VIILANI <alexandre.villani@edu.univ-fcomte.fr>
2026-07-28 11:53:03 +0200

    feat(client): Ajout de la validation d'unicité sur le nom du client

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -5,8 +5,10 @@ namespace App\Entity;
 use App\Repository\ClientRepository;
 use Doctrine\DBAL\Types\Types; 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: ClientRepository::class)]
+#[UniqueEntity(fields: ['nom'], message: 'Une société portant ce nom existe déjà dans la base de données.')]
 class Client
 {
 
