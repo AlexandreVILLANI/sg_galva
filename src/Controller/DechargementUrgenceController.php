545b52443ea0980eb23b960ee148106cae9c5ff9
Alexandre VIILANI <alexandre.villani@edu.univ-fcomte.fr>
2026-07-30 14:03:36 +0200

    feat(cariste): Ajout des fonctionnalités d'affichage (show) et suppression pour les déchargements d'urgence

--- src/Controller/DechargementUrgenceController.php
+++ src/Controller/DechargementUrgenceController.php
@@ -86,5 +86,23 @@ class DechargementUrgenceController extends AbstractController
                 }
             }
         }
+    #[Route('/{id}', name: 'app_urgence_show', methods: ['GET'])]
+    public function show(DechargementUrgence $urgence): Response
+    {
+        return $this->render('urgence/show.html.twig', [
+            'urgence' => $urgence,
+        ]);
+    }
+
+    #[Route('/{id}/delete', name: 'app_urgence_delete', methods: ['POST'])]
+    public function delete(Request $request, DechargementUrgence $urgence, EntityManagerInterface $em): Response
+    {
+        if ($this->isCsrfTokenValid('delete_urgence' . $urgence->getId(), $request->request->get('_token'))) {
+            $em->remove($urgence);
+            $em->flush();
+            $this->addFlash('success', 'Le déchargement d\'urgence a été supprimé.');
+        }
+
+        return $this->redirectToRoute('app_home', ['section' => 'cariste-urgence']);
     }
 }
